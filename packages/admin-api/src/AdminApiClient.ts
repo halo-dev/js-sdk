2179608cb694f1375a9008d2683547111680d52a
guqing <1484563614@qq.com>
2021-06-17 22:17:07 +0800

    feat: Add user client methods for admin api

--- packages/admin-api/src/AdminApiClient.ts
+++ packages/admin-api/src/AdminApiClient.ts
@@ -1,10 +1,6 @@
 import { HaloRestAPIClient, HttpClient } from "@guching/rest-api-client";
 import { buildPath } from "./url";
-import {
-  Environment,
-  Response,
-  AccessToken,
-} from "./types";
+import { Environment, Response, AccessToken } from "./types";
 import {
   AttachmentClient,
   BackupClient,
@@ -24,52 +20,58 @@ import {
   SheetCommentClient,
   SheetClient,
   StatisticClient,
-  TagClient
-} from './clients'
+  TagClient,
+  ThemeClient,
+  UserClient,
+} from "./clients";
 
 export class AdminApiClient {
   private client: HttpClient;
-  private _attachment: AttachmentClient
-  private _backup: BackupClient
-  private _category: CategoryClient
-  private _installation: InstallationClient
-  private _journalComment: JournalCommentClient
-  private _journal: JournalClient
-  private _link: LinkClient
-  private _log: LogClient
-  private _mail: MailClient
-  private _menu: MenuClient
-  private _migration: MigrationClient
-  private _option: OptionClient
-  private _photo: PhotoClient
-  private _postComment: PostCommentClient
-  private _post: PostClient
-  private _sheetComment: SheetCommentClient
-  private _sheet: SheetClient
-  private _statistic: StatisticClient
-  private _tag: TagClient
+  private _attachment: AttachmentClient;
+  private _backup: BackupClient;
+  private _category: CategoryClient;
+  private _installation: InstallationClient;
+  private _journalComment: JournalCommentClient;
+  private _journal: JournalClient;
+  private _link: LinkClient;
+  private _log: LogClient;
+  private _mail: MailClient;
+  private _menu: MenuClient;
+  private _migration: MigrationClient;
+  private _option: OptionClient;
+  private _photo: PhotoClient;
+  private _postComment: PostCommentClient;
+  private _post: PostClient;
+  private _sheetComment: SheetCommentClient;
+  private _sheet: SheetClient;
+  private _statistic: StatisticClient;
+  private _tag: TagClient;
+  private _theme: ThemeClient;
+  private _user: UserClient;
 
   constructor(client: HaloRestAPIClient) {
     this.client = client.buildHttpClient();
-    this._attachment = new AttachmentClient(this.client)
-    this._backup = new BackupClient(this.client)
-    this._category = new CategoryClient(this.client)
-    this._installation = new InstallationClient(this.client)
-    this._journalComment = new JournalCommentClient(this.client)
-    this._journal = new JournalClient(this.client)
-    this._link = new LinkClient(this.client)
-    this._log = new LogClient(this.client)
-    this._mail = new MailClient(this.client)
-    this._menu = new MenuClient(this.client)
-    this._migration = new MigrationClient(this.client)
-    this._option = new OptionClient(this.client)
-    this._photo = new PhotoClient(this.client)
-    this._postComment = new PostCommentClient(this.client)
-    this._post = new PostClient(this.client)
-    this._sheetComment = new SheetCommentClient(this.client)
-    this._sheet = new SheetClient(this.client)
-    this._statistic = new StatisticClient(this.client)
-    this._tag = new TagClient(this.client)
+    this._attachment = new AttachmentClient(this.client);
+    this._backup = new BackupClient(this.client);
+    this._category = new CategoryClient(this.client);
+    this._installation = new InstallationClient(this.client);
+    this._journalComment = new JournalCommentClient(this.client);
+    this._journal = new JournalClient(this.client);
+    this._link = new LinkClient(this.client);
+    this._log = new LogClient(this.client);
+    this._mail = new MailClient(this.client);
+    this._menu = new MenuClient(this.client);
+    this._migration = new MigrationClient(this.client);
+    this._option = new OptionClient(this.client);
+    this._photo = new PhotoClient(this.client);
+    this._postComment = new PostCommentClient(this.client);
+    this._post = new PostClient(this.client);
+    this._sheetComment = new SheetCommentClient(this.client);
+    this._sheet = new SheetClient(this.client);
+    this._statistic = new StatisticClient(this.client);
+    this._tag = new TagClient(this.client);
+    this._theme = new ThemeClient(this.client);
+    this._user = new UserClient(this.client);
   }
 
   public getEnvironment(): Promise<Response<Environment>> {
@@ -101,10 +103,10 @@ export class AdminApiClient {
   }
 
   public sendResetPasswordCode(params: {
-    username: string
-    email: string
-    code?: string
-    password?: string
+    username: string;
+    email: string;
+    code?: string;
+    password?: string;
   }): void {
     const path = buildPath({
       endpointName: "password/code",
@@ -113,10 +115,10 @@ export class AdminApiClient {
   }
 
   public resetPassword(params: {
-    username: string
-    email: string
-    code?: string
-    password?: string
+    username: string;
+    email: string;
+    code?: string;
+    password?: string;
   }): void {
     const path = buildPath({
       endpointName: "password/reset",
@@ -132,78 +134,86 @@ export class AdminApiClient {
   }
 
   public get attachment() {
-    return this._attachment
+    return this._attachment;
   }
 
   public get backup() {
-    return this._backup
+    return this._backup;
   }
 
   public get category() {
-    return this._category
+    return this._category;
   }
 
   public get installation() {
-    return this._installation
+    return this._installation;
   }
 
   public get journalComment() {
-    return this._journalComment
+    return this._journalComment;
   }
 
   public get journal() {
-    return this._journal
+    return this._journal;
   }
 
   public get link() {
-    return this._link
+    return this._link;
   }
 
   public get log() {
-    return this._log
+    return this._log;
   }
 
   public get mail() {
-    return this._mail
+    return this._mail;
   }
 
   public get menu() {
-    return this._menu
+    return this._menu;
   }
 
   public get migration() {
-    return this._migration
+    return this._migration;
   }
 
   public get option() {
-    return this._option
+    return this._option;
   }
 
   public get photo() {
-    return this._photo
+    return this._photo;
   }
 
   public get postComment() {
-    return this._postComment
+    return this._postComment;
   }
 
   public get post() {
-    return this._post
+    return this._post;
   }
 
   public get sheetComment() {
-    return this._sheetComment
+    return this._sheetComment;
   }
 
   public get sheet() {
-    return this._sheet
+    return this._sheet;
   }
 
   public get statistic() {
-    return this._statistic
+    return this._statistic;
   }
 
   public get tag() {
-    return this._tag
+    return this._tag;
+  }
+
+  public get theme() {
+    return this._theme;
+  }
+
+  public get user() {
+    return this._user;
   }
 }
