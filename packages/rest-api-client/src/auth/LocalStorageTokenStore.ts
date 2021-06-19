f312621ec0906eab477320325905fef36643e112
guqing <1484563614@qq.com>
2021-06-20 01:52:37 +0800

    feat: Add local storage token store implementation

---
+++ packages/rest-api-client/src/auth/LocalStorageTokenStore.ts
@@ -0,0 +1,23 @@
+import { TokenStore } from "../auth/TokenStore";
+import { AccessToken } from "../auth/CredentialsInterface";
+import store from "store";
+
+export class LocalStorageTokenStore implements TokenStore {
+  private tokenKey: string;
+
+  constructor(tokenKey: string = "ACCESS_TOKEN") {
+    this.tokenKey = tokenKey;
+  }
+
+  set(token: AccessToken): void {
+    store.set(this.tokenKey, token);
+  }
+
+  get(): Promise<AccessToken> {
+    return Promise.resolve(store.get(this.tokenKey));
+  }
+
+  clear(): void {
+    store.remove(this.tokenKey);
+  }
+}
