66127f5a962a50d9a2e76048c5a46bebe0f9d956
Ryan Wang <i@ryanc.cc>
2021-07-04 19:04:06 +0800

    refactor: admin theme client.

--- packages/admin-api/src/clients/ThemeClient.ts
+++ packages/admin-api/src/clients/ThemeClient.ts
@@ -9,21 +9,39 @@ export class ThemeClient {
     this.client = client;
   }
 
-  public listAll(): Promise<Response<Array<ThemeProperty>>> {
+  /**
+   * List all themes.
+   *
+   * @returns array of ThemeProperty
+   */
+  public list(): Promise<Response<Array<ThemeProperty>>> {
     const path = buildPath({
       endpointName: "themes",
     });
     return this.client.get(path, {});
   }
 
-  public getById(themeId: string): Promise<Response<ThemeProperty>> {
+  /**
+   * Get theme property by themeId.
+   *
+   * @param themeId themeId
+   * @returns ThemeProperty
+   */
+  public get(themeId: string): Promise<Response<ThemeProperty>> {
     const path = buildPath({
       endpointName: `themes/${themeId}`,
     });
     return this.client.get(path, {});
   }
 
-  public deleteById(
+  /**
+   * Delete theme by themeId.
+   *
+   * @param themeId themeId
+   * @param deleteSettings whether to delete the theme settings at the same time.
+   * @returns ThemeProperty
+   */
+  public delete(
     themeId: string,
     deleteSettings?: boolean
   ): Promise<Response<ThemeProperty>> {
@@ -33,6 +51,12 @@ export class ThemeClient {
     return this.client.delete(path, {});
   }
 
+  /**
+   * Active a theme.
+   *
+   * @param themeId themeId
+   * @returns ThemeProperty
+   */
   public active(themeId: string): Promise<Response<ThemeProperty>> {
     const path = buildPath({
       endpointName: `themes/${themeId}/activation`,
@@ -40,42 +64,79 @@ export class ThemeClient {
     return this.client.post(path, {});
   }
 
-  public fetchConfig(): Promise<Response<Array<Group>>> {
+  /**
+   * List activated theme configurations.
+   *
+   * @returns array of configuration group.
+   */
+  public listActivatedConfigurations(): Promise<Response<Array<Group>>> {
     const path = buildPath({
       endpointName: "themes/activation/configurations",
     });
     return this.client.get(path, {});
   }
 
-  public fetchConfigById(themeId: string): Promise<Response<Array<Group>>> {
+  /**
+   * List theme configurations by themeId.
+   *
+   * @param themeId themeId
+   * @returns array of configuration group.
+   */
+  public listConfigurations(themeId: string): Promise<Response<Array<Group>>> {
     const path = buildPath({
       endpointName: `themes/${themeId}/configurations`,
     });
     return this.client.get(path, {});
   }
 
-  public listFilesById(themeId: string): Promise<Response<Array<ThemeFile>>> {
+  /**
+   * List theme files by themeId.
+   *
+   * @param themeId themeId
+   * @returns arrary of ThemeFile
+   */
+  public listFiles(themeId: string): Promise<Response<Array<ThemeFile>>> {
     const path = buildPath({
       endpointName: `themes/${themeId}/files`,
     });
     return this.client.get(path, {});
   }
 
-  public listFiles(): Promise<Response<Array<ThemeFile>>> {
+  /**
+   * List activated theme files.
+   *
+   * @returns arrary of ThemeFile
+   */
+  public listActivatedFiles(): Promise<Response<Array<ThemeFile>>> {
     const path = buildPath({
       endpointName: "themes/activation/files",
     });
     return this.client.get(path, {});
   }
 
-  public getContentByPath(filepath: string): Promise<Response<string>> {
+  /**
+   * Get activated template content by filepath.
+   *
+   * @param filepath filepath
+   * @returns template content
+   */
+  public getActivatedTemplateContent(
+    filepath: string
+  ): Promise<Response<string>> {
     const path = buildPath({
       endpointName: "themes/files/content",
     });
     return this.client.get(path, { path: filepath });
   }
 
-  public getContentById(
+  /**
+   * Get template content by themeId and filepath.
+   *
+   * @param themeId themeId
+   * @param filepath filepath
+   * @returns template content
+   */
+  public getTemplateContent(
     themeId: string,
     filepath: string
   ): Promise<Response<string>> {
@@ -85,7 +146,13 @@ export class ThemeClient {
     return this.client.get(path, { path: filepath });
   }
 
-  public async updateContentById(
+  /**
+   * Update theme template content by themeId.
+   *
+   * @param themeId themeId
+   * @param params path, content
+   */
+  public async updateTemplateContent(
     themeId: string,
     params: {
       path?: string;
@@ -98,23 +165,40 @@ export class ThemeClient {
     await this.client.put(path, { ...params });
   }
 
-  public listSettingsById(
+  /**
+   * List theme settings by themeId.
+   *
+   * @param themeId themeId
+   * @returns Record<string, unknown>
+   */
+  public listSettings(
     themeId: string
-  ): Promise<Response<Map<String, any>>> {
+  ): Promise<Response<Record<string, unknown>>> {
     const path = buildPath({
       endpointName: `themes/${themeId}/settings`,
     });
     return this.client.get(path, {});
   }
 
-  public listSettings(): Promise<Response<Map<String, any>>> {
+  /**
+   * List activated theme settings.
+   *
+   * @returns Record<string, unknown>
+   */
+  public listActivatedSettings(): Promise<Response<Record<string, unknown>>> {
     const path = buildPath({
       endpointName: "themes/activation/settings",
     });
     return this.client.get(path, {});
   }
 
-  public async saveSettingsById(
+  /**
+   * Save settings by themeId.
+   *
+   * @param themeId themeId
+   * @param settings settings
+   */
+  public async saveSettings(
     themeId: string,
     settings: Map<String, Object>
   ): Promise<void> {
@@ -124,34 +208,62 @@ export class ThemeClient {
     await this.client.post(path, { settings });
   }
 
-  public async saveSettings(settings: Map<String, Object>): Promise<void> {
+  /**
+   * Save activated theme settings.
+   *
+   * @param settings settings
+   */
+  public async saveActivatedSettings(
+    settings: Map<String, Object>
+  ): Promise<void> {
     const path = buildPath({
       endpointName: "themes/activation/settings",
     });
     await this.client.post(path, { settings });
   }
 
-  public getActivateTheme(): Promise<Response<ThemeProperty>> {
+  /**
+   * Get activated theme property.
+   *
+   * @returns ThemeProperty
+   */
+  public getActivatedTheme(): Promise<Response<ThemeProperty>> {
     const path = buildPath({
       endpointName: "themes/activation",
     });
     return this.client.get(path, {});
   }
 
-  public getCustomPostTemplate(): Promise<Response<Array<String>>> {
+  /**
+   * List activated theme custom post templates.
+   *
+   * @returns array of template name.
+   */
+  public listCustomPostTemplates(): Promise<Response<Array<String>>> {
     const path = buildPath({
       endpointName: "themes/activation/template/custom/post",
     });
     return this.client.get(path, {});
   }
 
-  public getCustomSheetTemplate(): Promise<Response<Array<String>>> {
+  /**
+   * List activated theme custom sheet templates.
+   *
+   * @returns array of template name.
+   */
+  public listCustomSheetTemplates(): Promise<Response<Array<String>>> {
     const path = buildPath({
       endpointName: "themes/activation/template/custom/sheet",
     });
     return this.client.get(path, {});
   }
 
+  /**
+   * Check template exists
+   *
+   * @param template template name
+   * @returns boolean
+   */
   public exists(template: string): Promise<Response<boolean>> {
     const path = buildPath({
       endpointName: "themes/activation/template/exists",
@@ -159,35 +271,69 @@ export class ThemeClient {
     return this.client.get(path, { template });
   }
 
-  public fetchTheme(uri: string) {
+  /**
+   * Fetch theme by uri
+   *
+   * @param uri uri
+   * @returns void
+   */
+  public fetchTheme(uri: string): Promise<Response<ThemeProperty>> {
     const path = buildPath({
       endpointName: "themes/fetching",
     });
     return this.client.get(path, { uri });
   }
 
-  public updateThemeByFetching(themeId: string) {
+  /**
+   * update theme by fetch.
+   *
+   * @param themeId themeId
+   * @returns void
+   */
+  public updateThemeByFetching(
+    themeId: string
+  ): Promise<Response<ThemeProperty>> {
     const path = buildPath({
       endpointName: `themes/fetching/${themeId}`,
     });
     return this.client.put(path, {});
   }
 
-  public updateContent(params: { path?: string; content?: string }) {
+  /**
+   * Update activated theme template content.
+   *
+   * @param params path, content
+   * @returns void
+   */
+  public updateActivatedTemplateContent(params: {
+    path?: string;
+    content?: string;
+  }): Promise<Response<void>> {
     const path = buildPath({
       endpointName: "themes/files/content",
     });
     return this.client.put(path, { ...params });
   }
 
-  public async reload(): Promise<void> {
+  /**
+   * Refresh theme list cache.
+   *
+   * @returns void
+   */
+  public reload(): Promise<Response<void>> {
     const path = buildPath({
       endpointName: "themes/reload",
     });
-    await this.client.post(path, {});
+    return this.client.post(path, {});
   }
 
-  public uploadTheme(data: unknown): Promise<Response<ThemeProperty>> {
+  /**
+   * Upload a theme.
+   *
+   * @param data data
+   * @returns ThemeProperty
+   */
+  public upload(data: unknown): Promise<Response<ThemeProperty>> {
     const path = buildPath({
       endpointName: "themes/upload",
     });
@@ -196,7 +342,14 @@ export class ThemeClient {
     return this.client.post(path, formData);
   }
 
-  public updateThemeByUpload(
+  /**
+   * Update theme by upload.
+   *
+   * @param themeId themeId
+   * @param data data
+   * @returns ThemeProperty
+   */
+  public updateByUpload(
     themeId: string,
     data: unknown
   ): Promise<Response<ThemeProperty>> {
