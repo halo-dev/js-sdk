e9d6f60c982ca77d2809fd688d2f4c3b5ed5bb74
Ryan Wang <i@ryanc.cc>
2022-02-20 22:37:39 +0800

    feat: BasePostSimple type adds isInProcess field (#22)
    * feat: BasePostSimple type adds isInProcess field
    
    Signed-off-by: Ryan Wang <i@ryanc.cc>
    
    * refactor: isInProgress to inProgress
    
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- packages/admin-api/src/types/index.ts
+++ packages/admin-api/src/types/index.ts
@@ -140,6 +140,7 @@ export interface BasePostSimple extends BasePostMinimal {
   likes: number
   wordCount: number
   isTopped: boolean
+  inProcess: boolean
 }
 
 export interface BasePostDetail extends BasePostSimple {
